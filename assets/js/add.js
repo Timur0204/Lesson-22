a52eed83ac0017fd6b378331ee9e2b108b23c04d
Timur0204 <timur02042008@gmail.com>
2023-11-30 21:38:26 +0200

    fixed

--- assets/js/add.js
+++ assets/js/add.js
@@ -3,28 +3,29 @@
         let height = +userHeight.value;
         let weight = +userWeight.value;
         let bodyMassIndex = weight / height **2;
+        let diagnosis = '...';
         
         if(bodyMassIndex <= 16) {
-            addedInfo = 'Дефицит массы тела';
+            diagnosis = 'Дефицит массы тела';
         } else if(bodyMassIndex >= 16 && bodyMassIndex < 18.5) {
-            addedInfo = 'Недостаточная (дефицит) масса тела';
+            diagnosis = 'Недостаточная (дефицит) масса тела';
         } else if(bodyMassIndex >= 18.5 && bodyMassIndex < 25) {
-            addedInfo = 'Нормальная масса тела';
+            diagnosis = 'Нормальная масса тела';
         } else if (bodyMassIndex >= 25 && bodyMassIndex < 30) {
-            addedInfo = 'Избыточная масса тела (предожирение)';
+            diagnosis = 'Избыточная масса тела (предожирение)';
         } else if(bodyMassIndex >= 30 && bodyMassIndex < 35) {
-            addedInfo = 'Ожирение 1 степени';
+            diagnosis = 'Ожирение 1 степени';
         } else if(bodyMassIndex >= 35 && bodyMassIndex < 40) {
-            addedInfo = 'Ожирение 2 степени';
+            diagnosis = 'Ожирение 2 степени';
         } else if(bodyMassIndex >= 40) {
-            addedInfo = 'Ожирение 3 степени'
+            diagnosis = 'Ожирение 3 степени'
         }
 
         bodyMassIndex = Math.round(bodyMassIndex * 10) / 10;
         
 
         result.innerHTML = bodyMassIndex;
-        addedInfo.innerHTML = addedInfo;
+        addedInfo.innerHTML = diagnosis;
     }
 
     
